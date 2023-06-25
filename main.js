7dd12f7aeb5d80c64325cddf8bfd837f8d164875
NoobHamada <100050458+NoobHamada@users.noreply.github.com>
2023-06-25 12:59:48 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -6,6 +6,6 @@ allLis.forEach(function(el) {
             el.style.display = "none"
         })
         this.style.display = "block",
-        this.style.border = "2px red solid"
+        this.style.border = "5px red solid"
     }
 });
